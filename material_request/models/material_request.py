f2f7150756857bc63f1b1faea67efec902bc062d
Gowtham-1910 <gowthammurugesan760@gmail.com>
2025-11-20 23:21:30 +0530

    forecast removed

--- material_request/models/material_request.py
+++ material_request/models/material_request.py
@@ -63,6 +63,7 @@ class MaterialRequest(models.Model):
         return self.false_
 
     @api.model_create_multi
+    
     def create(self, values):
         for vals in values:
             vals['name'] = self.env['ir.sequence'].next_by_code('material.request.sequence')
