646b143de3c886d70daf6012c78c369f2d6bcf1d
Gowtham-1910 <gowthammurugesan760@gmail.com>
2026-01-23 18:06:00 +0530

    [update]

--- odoo_website_helpdesk/__manifest__.py
+++ odoo_website_helpdesk/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Website Helpdesk Management",
-    'version': '18.0.1.0.3',
+    'version': '18.0.1.0.4',
     'category': 'Website',
     'summary': "Create and manage helpdesk tickets from website and backend.",
     'description': """Website Helpdesk Management for Odoo 18 with ticket creation, 
--- odoo_website_helpdesk/models/res_partner.py
+++ odoo_website_helpdesk/models/res_partner.py
@@ -3,5 +3,5 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    ticket_type_id = fields.Many2one('helpdesk.type', string='Machine name', required=True, help='Ticket Type')
+    ticket_type_id = fields.Many2one('helpdesk.type', string='Machine name', help='Ticket Type')
     on_time_rate = fields.Float(string='On Time Rate', help='On-time delivery or service rate percentage')
